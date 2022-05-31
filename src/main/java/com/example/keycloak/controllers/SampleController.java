567878560517e81addb862dd4be38b6ffc665218
i.gordienko <i.gordienko@ylab.io>
2022-05-31 13:51:12 +0300

    Добавил структуру

--- src/main/java/com/example/keycloak/controllers/SampleController.java
+++ src/main/java/com/example/keycloak/controllers/SampleController.java
@@ -1,4 +1,4 @@
-package com.example.keycloak;
+package com.example.keycloak.controllers;
 
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.security.core.Authentication;
