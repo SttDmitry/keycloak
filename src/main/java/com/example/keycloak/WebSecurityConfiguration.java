b513f1b2c43394c9e1622b99a55cd0c3d6cee5ee
Шейко Дмитрий Алексеевич <sheicko.dmitry@yandex.ru>
2022-05-31 09:54:52 +0300

    Fixed getting set of authorities

--- src/main/java/com/example/keycloak/WebSecurityConfiguration.java
+++ src/main/java/com/example/keycloak/WebSecurityConfiguration.java
@@ -5,7 +5,6 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.oauth2.client.oidc.userinfo.OidcUserRequest;
@@ -13,6 +12,7 @@ import org.springframework.security.oauth2.client.oidc.userinfo.OidcUserService;
 import org.springframework.security.oauth2.client.userinfo.OAuth2UserService;
 import org.springframework.security.oauth2.core.oidc.user.DefaultOidcUser;
 import org.springframework.security.oauth2.core.oidc.user.OidcUser;
+import org.springframework.security.web.SecurityFilterChain;
 
 import java.util.Map;
 import java.util.Set;
@@ -20,10 +20,10 @@ import java.util.stream.Collectors;
 
 @Configuration
 @EnableGlobalMethodSecurity(prePostEnabled = true)
-public class WebSecurityConfiguration extends WebSecurityConfigurerAdapter {
+public class WebSecurityConfiguration {
 
-    @Override
-    protected void configure(HttpSecurity http) throws Exception {
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         http
                 .authorizeRequests(authorizeRequests -> authorizeRequests
                         .antMatchers("/api/anonymous/**").permitAll()
@@ -33,7 +33,7 @@ public class WebSecurityConfiguration extends WebSecurityConfigurerAdapter {
                                 .oidcUserService(this.oidcUserService())
                         )
                 );
-
+        return http.build();
     }
 
     @Bean
@@ -43,11 +43,8 @@ public class WebSecurityConfiguration extends WebSecurityConfigurerAdapter {
         return (userRequest) -> {
             OidcUser oidcUser = delegate.loadUser(userRequest);
 
-            final Map<String, Object> claims = oidcUser.getClaims();
-            final JSONArray groups = (JSONArray) claims.get("groups");
-
-            final Set<GrantedAuthority> mappedAuthorities = groups.stream()
-                    .map(role -> new SimpleGrantedAuthority(("ROLE_" + role)))
+            final Set<GrantedAuthority> mappedAuthorities = oidcUser.getAuthorities().stream()
+                    .map(role -> new SimpleGrantedAuthority(("" + role)))
                     .collect(Collectors.toSet());
 
             return new DefaultOidcUser(mappedAuthorities, oidcUser.getIdToken(), oidcUser.getUserInfo());
